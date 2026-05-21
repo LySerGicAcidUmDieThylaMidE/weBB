ae165ee3ce13c835e5afeb61ec75877f10b4ddc3
LySerGicAcidUmDieThylaMidE <poslednii93@gmail.com>
2026-05-21 10:39:59 +0300

    feat: add in app.py frontend route, error route and running block of code

--- app.py
+++ app.py
@@ -382,35 +382,27 @@ def remove_favorite(toy_id):
     })
 
 
+# ------------------ front ------------------
 
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-
-    # регистрация роутов
-    from routes.auth import auth_bp
-    from routes.toys import toys_bp
-    from routes.cart import cart_bp
-    from routes.orders import orders_bp
-
-    app.register_blueprint(auth_bp, url_prefix="/api")
-    app.register_blueprint(toys_bp, url_prefix="/api")
-    app.register_blueprint(cart_bp, url_prefix="/api")
-    app.register_blueprint(orders_bp, url_prefix="/api")
-
-    return app
-
-
-app = create_app()
-
-from flask import render_template
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    return send_from_directory("static", "index.html")
+
+# ------------------ error ------------------
+
+@app.errorhandler(404)
+def not_found(e):
+
+    return jsonify({
+        "error":"Not found"
+    }), 404
+
+# ------------------ main ------------------
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
