948d576d86e6c667651dff87690b70ba363de64e
LySerGicAcidUmDieThylaMidE <poslednii93@gmail.com>
2026-05-21 10:32:29 +0300

    feat: add in app.py --models--

--- app.py
+++ app.py
@@ -13,6 +13,52 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# ------------------ models ------------------
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.Integer, default=0)
+
+
+class Toy(db.Model):
+    __tablename__ = "toys"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    price = db.Column(db.Numeric(10, 2))
+    manufacturer = db.Column(db.String(255))
+    quantity = db.Column(db.Integer)
+    min_age = db.Column(db.Integer)
+    image_url = db.Column(db.String(500))
+    category = db.Column(db.String(100))
+
+
+class CartItem(db.Model):
+    __tablename__ = "cart"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    toy_id = db.Column(db.Integer)
+    quantity = db.Column(db.Integer, default=1)
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer)
+    toy_id = db.Column(db.Integer)
+
+
+
+
+
+
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
