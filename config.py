d044c877c38b921099b070fc559a318c7bb836d6
LySerGicAcidUmDieThylaMidE <poslednii93@gmail.com>
2026-05-21 10:46:02 +0300

    style: cancel 1 enter

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1111@localhost/luderezone"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
