5bf792c4af0dc8a4c60ab2799a27b7a90c31d806
LySerGicAcidUmDieThylaMidE <poslednii93@gmail.com>
2026-06-04 10:56:02 +0300

    feat: secret_word

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    secret_word = db.Column(db.String(255), nullable=False)
     role = db.Column(db.Integer, default=0)
 
 
