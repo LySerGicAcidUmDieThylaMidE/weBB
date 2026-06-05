ef64e57855834cac15b7882246813a9995f4343d
LySerGicAcidUmDieThylaMidE <poslednii93@gmail.com>
2026-06-06 02:59:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://3r2eCYwycnYB3x1.root:I5I10um2rA3CLot2@gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/sys"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://root@localhost/my_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
